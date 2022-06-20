8d4ca0779656f650eabf197b578049cee1279b55
ropicaza <rocio.picaza@gmail.com>
2022-06-20 12:05:37 -0300

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-    validates :email, uniqueness: true
-    validates_format_of :email, with: /@/
-    validates :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates :password_digest, presence: true
+
+  has_secure_password
 end
