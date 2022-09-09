3a1e04b0f7eb7fa9f4a7837a6c9d368566290c3b
Rocio Picaza <rocio.picaza@gmail.com>
2022-09-09 10:40:30 -0300

    Finishes modeling the product model along with user associations

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -3,5 +3,18 @@ require "test_helper"
 class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
-    end
+  end
+
+  test "should show products" do
+    get api_v1_product_url(), as: :json
+    assert_response :success
+  end
+
+  test "should show product" do
+    get api_v1_product_url(@product), as: :json
+    assert_response :success
+
+    json_response = JSON.parse(self.response.body)
+    assert_equal @product.title, json_response['title']
+  end
 end
