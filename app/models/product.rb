3ec3b3b37ae29fd0d8892c790384774df2a674dc
Rocio Picaza <rocio.picaza@gmail.com>
2022-09-12 17:41:39 -0300

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,16 @@ class Product < ApplicationRecord
   scope :below_or_equal_to_price, lambda { |price| where('price <= ?', price) }
   scope :recent, lambda { order(:updated_at) }
 
+
+  def self.search(params = {})
+    products = params[:products_ids].present? ? Product.where(id: params[:products_ids]) : Product.all
+
+    products = products.filter_by_title(params[:keyword]) if params[:keyword]
+    products = products.above_or_equal_to_price(params[:min_price].to_f) if params[:min_price]
+    products = products.below_or_equal_to_price(params[:max_price].to_f) if params[:max_price]
+    products = products.recent if params[:recent]
+
+    products
+  end
+
 end
