96c40b15523e536d8fd8ea8a9c878ccd87af179b
Rocio Picaza <rocio.picaza@gmail.com>
2022-09-13 12:35:13 -0300

    Adds the create method for the orders controller

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,10 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
+  setup do
+    @order = orders(:one)
+    @product1 = products(:one)
+    @product2 = products(:two)
+  end
 
-  test 'should have a positive total' do
-    order = orders(:one)
-    order.total = -1
-    assert_not order.valid?
+  test 'should set total' do
+    order = Order.new user_id: @order.user_id
+    order.products << products(:one)
+    order.products << products(:two)
+    order.save
+    assert_equal (@product1.price + @product2.price), order.set_total!
   end
 end
