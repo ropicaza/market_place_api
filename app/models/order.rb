4b68c8db4056e8f18fabc889b5d13ca34092d9f0
Rocio Picaza <rocio.picaza@gmail.com>
2022-09-13 14:44:32 -0300

    Allows the order to be placed along with product quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,8 +8,14 @@ class Order < ApplicationRecord
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
 
-
   def set_total!
     self.total = products.sum :price
+  end
+
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      placement = placements.build(product_id: product_id_and_quantity[:product_id])
+      yield placement if block_given?
+    end
   end
 end
