e033ab1fc9eca2a6d92abeafc210bee43e482835
Rocio Picaza <rocio.picaza@gmail.com>
2022-09-14 09:45:19 -0300

    Adds caching for the serializers

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,4 +3,6 @@ class ProductSerializer
   attributes :title, :price, :published
 
   belongs_to :user
+
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,6 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :email
   has_many :products
+
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
 end
